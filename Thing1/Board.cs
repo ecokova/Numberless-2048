45dea72a35a560191044b2b1efd9b727ce21469f
ecokova <elena.cokova@tufts.edu>
2015-12-24 18:54:49 -0500

    init commit

---
+++ Thing1/Board.cs
@@ -0,0 +1,219 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Thing1
+{
+    public class Board
+    {
+        private Texture2D imgBoard;
+        private Texture2D imgTile;
+
+        private struct Tile
+        {
+            public int value;
+            public int row;
+            public int col;
+            public int targetRow;
+            public int targetCol;
+            public Vector2 currentPos;
+            public Boolean isMoving;
+        };
+
+        #region Array of colors
+        private Color[] colors = {   
+                             new Color(208,194,183), 
+                             new Color(180, 157, 139), 
+                             new Color(226, 82, 85),
+                             new Color(245, 160, 77),
+                             new Color(240, 225, 110),
+                             new Color(182, 230, 120),
+                             new Color(127, 206, 127),
+                             new Color(108, 181, 161),
+                             new Color(95, 154, 184),
+                             new Color(136, 136, 196),
+                             new Color(164, 124, 207),
+                             new Color(191, 138, 191),
+                             new Color(204, 125, 141)
+                         };
+        #endregion
+
+        Tile NULLTILE;
+        private Vector2[,] positions;
+        private List<Tile> tiles;
+
+        public Boolean tilesMoving;
+
+        // Constructor / implicit "Initialize" method
+        public Board()
+        {
+            createPositionsArray();
+            createStartingBoard();
+            tilesMoving = false;
+        }
+
+        // Creates and initializes array of positions coordinates
+        private void createPositionsArray()
+        {
+            positions = new Vector2[4, 4];
+            
+            for (int row = 0; row < 4; row++)
+            {
+                for (int col = 0; col < 4; col++)
+                {
+                    //                                pixel border + tile width    
+                    positions[row, col] = new Vector2((2 * (col + 1)) + (100 * col), (2 * (row + 1)) + (100 * row));
+                }
+            }
+        }
+        // Creates tiles list and fills with 8 randomized tiles
+        private void createStartingBoard()
+        {
+            tiles = new List<Tile>();
+
+            
+            //Start with 8 tiles
+            Random random = new Random();
+            while (tiles.Count() < 8)
+            {
+                int pos = random.Next(0, 16);
+                while (tiles.Any<Tile>(t => t.col + t.row * 4 == pos))
+                {
+                    pos = random.Next(0, 16);
+                }
+
+                Tile tile = new Tile();
+                tile.value = random.Next(0, 2);
+                tile.row = pos / 4;
+                tile.col = pos % 4;
+                tile.targetRow = tile.row;
+                tile.targetCol = tile.col;
+                tile.isMoving = false;
+                tile.currentPos = positions[tile.row, tile.col];
+                //Console.WriteLine("Tile " + tiles.Count() + ": (" + tile.row + "," + tile.col + ")"); //% DEBUG
+
+                tiles.Add(tile);
+            }
+        }
+
+        // LoadContent method
+        public void LoadContent(ContentManager content)
+        {
+            imgBoard = content.Load<Texture2D>("board");
+            imgTile = content.Load<Texture2D>("tile");
+        }
+
+        // Update method
+        public void Update(Dir direction)
+        {
+            if (tilesMoving)
+            {
+                //Console.WriteLine("Tiles moving...");
+                moveTiles(direction); 
+            }
+            else if (direction != Dir.None)
+            {
+                
+                tilesMoving = true;
+                //Console.WriteLine("Tiles not moving.");
+                
+                for (int i = 0; i < tiles.Count; i++ )
+                {
+                    Tile temp = tiles[i];
+                    switch (direction)
+                    {
+                        case Dir.Up:
+                            temp.targetRow--;
+                            if (temp.targetRow < 0) temp.targetRow = 3;
+                            break;
+                        case Dir.Down:
+                            temp.targetRow = (temp.targetRow + 1) % 4;
+                            break;
+                        case Dir.Left:
+                            temp.targetCol--;
+                            if (temp.targetCol < 0) temp.targetCol = 3;
+                            break;
+                        case Dir.Right:
+                            temp.targetCol = (temp.targetCol + 1) % 4;
+                            break;
+                    }
+                    if (temp.targetCol == temp.col || temp.targetRow == temp.row)
+                        Console.WriteLine("*****************************************************************");
+                    temp.isMoving = true;
+                    tiles[i] = temp;
+                }
+            }
+        }
+
+        private void moveTiles(Dir _direction)
+        {
+            // Loops through tiles, for each moves the tile or marks it as done moving
+            for (int i = 0; i < tiles.Count; i++) 
+            {
+                Tile temp = tiles[i];
+                if (temp.Equals(NULLTILE))
+                    continue;
+                //Console.WriteLine("Tile " + i + ": (" + temp.row + "," + temp.col + ") => (" + temp.targetRow + "," + temp.targetCol + "), @ (" 
+                //    + (int)temp.currentPos.Y / 100 + "," + (int)temp.currentPos.X / 100 + ")");
+                if (tiles[i].currentPos == positions[tiles[i].targetRow, tiles[i].targetCol])
+                {
+                    temp.col = temp.targetCol;
+                    temp.row = temp.targetRow;
+                    temp.isMoving = false;
+
+                    foreach (Tile t in tiles.FindAll(t => t.currentPos == temp.currentPos && !t.Equals(temp))) { 
+                        //NEEDSWORK: Should it skip ones that aren't done moving yet?
+                        //Console.WriteLine(t.currentPos.ToString() + " vs " + temp.currentPos.ToString());
+                        
+                        temp.value = (temp.value + 1) % colors.Length; 
+                        tiles[tiles.IndexOf(t)] = NULLTILE;
+
+                    }
+                }
+                else
+                {
+                    //Console.WriteLine("Tile " + i + " moved");
+                    temp.currentPos += new Vector2((float)(tiles[i].targetRow - tiles[i].row) / 10.0f, 
+                        (float)(tiles[i].targetCol - tiles[i].col) / 10.0f);
+                }
+                tiles[i] = temp;
+            }
+            //Console.WriteLine("Done with loop");
+            tilesMoving = !tiles.TrueForAll(t => !t.isMoving);
+            //tiles.RemoveAll(t => t.Equals(NULLTILE));
+            
+
+        }
+        // Draw method
+        public void Draw(SpriteBatch spriteBatch, Vector2 offset)
+        {
+            //drawBoardDropShadow(spriteBatch, offset);
+
+            spriteBatch.Draw(imgBoard, offset, Color.White);
+            foreach (var t in tiles) {
+                spriteBatch.Draw(imgTile, t.currentPos + offset, colors[t.value]);
+            }
+        }
+
+        private void drawBoardDropShadow(SpriteBatch spriteBatch, Vector2 offset)
+        {
+            //spriteBatch.Draw(imgBoard., offset + new Vector2(2, 2), new Color(0, 0, 0, 32)); 
+
+            for (int i = -1; i < 2; i++)
+            {
+                for (int j = -1; j < 2; j++)
+                {
+                    spriteBatch.Draw(imgBoard, 
+                        new Rectangle((int)offset.X - 2 + i, (int)offset.Y - 2 + j, imgBoard.Width + 4, imgBoard.Height + 4),
+                        new Color(0, 0, 0, 32));
+                }
+            }
+                 
+        }
+        
+    }
+}
---
+++ Thing1/Game1.cs
@@ -0,0 +1,143 @@
+﻿#region Using Statements
+using System;
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.GamerServices;
+#endregion
+
+namespace Thing1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    /// 
+
+    public enum Dir
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        None
+    }
+
+    public class Game1 : Game
+    {
+        GraphicsDeviceManager graphics;
+        SpriteBatch spriteBatch;
+        Texture2D imgCat;
+        Board board;
+
+        int numAs;
+        Dir direction;
+
+        public Game1()
+            : base()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            numAs = 0;
+            board = new Board();
+            direction = Dir.None;
+
+            graphics.PreferredBackBufferWidth = 510;
+            graphics.PreferredBackBufferHeight = 570;
+            graphics.ApplyChanges();
+            
+            base.Initialize();
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+            imgCat = this.Content.Load<Texture2D>("cat");
+            board.LoadContent(this.Content);
+
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+            { numAs++; }
+
+            List<int> t = new List<int>();
+            //List<int>.Enumerator iter = t.GetEnumerator();
+          
+            if (!board.tilesMoving)
+            {
+                if (Keyboard.GetState().IsKeyDown(Keys.Up))
+                { direction = Dir.Up; }
+                else if (Keyboard.GetState().IsKeyDown(Keys.Down))
+                { direction = Dir.Down; }
+                else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+                { direction = Dir.Left; }
+                else if (Keyboard.GetState().IsKeyDown(Keys.Right))
+                { direction = Dir.Right; }
+                else
+                { direction = Dir.None; }
+            }
+            board.Update(direction);
+            base.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(35, 35, 35));
+            Vector2 offset = new Vector2(50, 150);
+            // TODO: Add your drawing code here
+
+            spriteBatch.Begin();
+
+            board.Draw(spriteBatch, offset);
+
+            spriteBatch.End();
+
+           
+
+            base.Draw(gameTime);
+        }
+    }
+}
